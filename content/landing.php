1ed0bc01350359c44a81926548a3f84be60843f0
waraker <email@example.com>
2013-11-01 10:58:16 +0000

    update landing page

--- content/landing.php
+++ content/landing.php
@@ -1,2 +1,3 @@
 <h1>Tweetbot Custom Image Service</h1>
 <p>Waiting...</p>
+<p>See: <a href="https://github.com/waraker/tweetbot-custom-image-service">github.com/waraker/tweetbot-custom-image-service</a> for more information</p>
