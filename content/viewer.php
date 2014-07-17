e28b49bb628f9c20cb959480daae86d0a0f75a0d
waraker <email@example.com>
2014-07-17 10:56:55 +0100

    Viewer: fix URLs, add error messages.

--- content/viewer.php
+++ content/viewer.php
@@ -1,43 +1,25 @@
 <?php
 
-$error_message = 'error';
-
-if(filter_var($_GET['url'], FILTER_VALIDATE_URL)){
+if (filter_var($_GET['url'], FILTER_VALIDATE_URL)) {
 
 	$image_url = $_GET['url'];
+	//error_log($image_url);
 	$url_data = parse_url($image_url);
+	//print_r($url_data);
 
-	// Only permit viewing of a local image
-	if($url_data['host'] == $_SERVER['SERVER_NAME']){
-
-		?>
+	// Permit viewing of a local image only.
+	if ($url_data['host'] == $_SERVER['SERVER_NAME']) { ?>
 
 <section id="view">
 <article>
-<h1><a href="/<?php
-
-		echo $image_url;
-
-		?>"><?php
-
-		echo $image_url;
-
-		?></a></h1>
-<a href="/<?php
-
-		echo $image_url;
-
-		?>"><img src="<?php
-
-		echo $image_url;
-
-		?>"></a>
+<h1><a href="<?php echo $image_url; ?>"><?php echo $image_url; ?></a></h1>
+<a href="<?php echo $image_url; ?>"><img src="<?php echo $image_url; ?>"></a>
 </article>
 </section><?php
 
 	}
-	else echo $error_message;
+	else echo 'Sorry, I can only show a local image.';
 }
-else echo $error_message;
+else echo 'Invalid image URL.';
 
 ?>
