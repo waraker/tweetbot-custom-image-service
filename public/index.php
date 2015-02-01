5426c4dad337a7c91c36ee4912ace893c3d300c8
waraker <ben@rmbp.prv>
2015-02-01 23:36:40 +0000

    Cleanup

--- public/index.php
+++ public/index.php
@@ -6,18 +6,18 @@ require'../req/function.php';
 
 $l = URL2Array();
 
-if(SITE_LIVE) $load_site = true;
-elseif(in_array($_SERVER['REMOTE_ADDR'], $admin_ip_address)) $load_site = true;
-else{
-	if($l) headerLocation();
+if (SITE_LIVE) $load_site = true;
+elseif (in_array($_SERVER['REMOTE_ADDR'], $admin_ip_address)) $load_site = true;
+else {
+	if ($l) headerLocation();
 	$load_site = false;
 }
 
-if($load_site){
+if ($load_site) {
 
 	require'../req/switch.php';
 
-	if(!$api){
+	if (!$api) {
 		require'../req/head.php';
 		require'../req/body.php';
 	}
